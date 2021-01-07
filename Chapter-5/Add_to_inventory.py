1d80a12c215e9c4766015f347f3c889c1c6bc2fa
siddharth424 <73775321+siddharth424@users.noreply.github.com>
2021-01-08 02:34:01 +0530

    add practice project Add to inventory

---
+++ Chapter-5/Add_to_inventory.py
@@ -0,0 +1,20 @@
+def addToInventory(inventory, addedItems):
+    for i in addedItems:
+        if i in inventory:
+            inventory[i] +=1
+        else:
+            inventory[i]=1
+    return inventory
+            
+def displayInventory(inventory):
+    print("Inventory:")
+    item_total = 0
+    for k, v in inventory.items():
+        print(str(v)+' '+k)
+        item_total += v
+    print("Total number of items: " + str(item_total))
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
